1917fa2abc5790c931b4a57fa04b48260d8efa29
shubhambhusari <shubhambhusari2@gmail.com>
2021-03-04 00:02:34 +0530

    Added:Given null mood should return happy

--- src/main/java/com/bridgelabz/moodanalyzerproblem/Moodanalyser.java
+++ src/main/java/com/bridgelabz/moodanalyzerproblem/Moodanalyser.java
@@ -3,30 +3,23 @@ package com.bridgelabz.moodanalyzerproblem;
 public class Moodanalyser {
 
     public String message;
-
-    public Moodanalyser(String message) {
+    public Moodanalyser(String message){
         this.message = message;
-
     }
-
     public String analyseMood() {
-
         try {
             if (message.contains("sad")) {
                 return "SAD";
-
             } else {
-
                 return "HAPPY";
-
             }
-        } catch (NullPointerException e) {
-            System.out.println("catch block executed");
         }
-        return "Exception got Handeled";
+        catch (NullPointerException e){
+            System.out.println(e);
+
+        }
+        return "Happy";
+
     }
+
 }
-
-
-
-
--- src/test/java/MoodanalyserTest.java
+++ src/test/java/MoodanalyserTest.java
@@ -7,16 +7,11 @@ public class MoodanalyserTest {
     Moodanalyser mood;
 
     @Test
-    public void testSadMood(){
+    public void testSadMood() {
 
         mood = new Moodanalyser(null);
 
         System.out.println(mood.analyseMood());
 
-    }
-    @Test
-    public void testHappyMood(){
-        mood = new Moodanalyser("I am in Happy mood");
-        System.out.println(mood.analyseMood());
     }
 }
